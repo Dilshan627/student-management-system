4576c0d28f48c8964b4674f60393642276f3eba8
dilshan27 <damithadilshan27@gmail.com>
2022-07-12 10:13:56 +0530

    add student model

--- src/controller/DashboardFormController.java
+++ src/controller/DashboardFormController.java
@@ -1,4 +1,28 @@
 package controller;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+
 public class DashboardFormController {
+    public TableView tblStudent;
+    public JFXTextField studentId;
+    public JFXTextField name;
+    public JFXTextField email;
+    public JFXTextField contact;
+    public JFXTextField address;
+    public JFXTextField nic;
+    public Button btnSave;
+
+
+    public void initialize(){
+
+    }
+
+    public void saveOnAction(ActionEvent actionEvent) {
+    }
+
+    public void DeleteOnAction(ActionEvent actionEvent) {
+    }
 }
